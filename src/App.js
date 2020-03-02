c76ca4aa54b385f00ae3f126efd94fdeebcf52ea
hagenbrandt <59567907+hagenbrandt@users.noreply.github.com>
2020-03-02 22:51:54 +0100

    Add image position control

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import DiveLogs from './DiveLog'
 import GlobalStyle from './common/GlobalStyles'
+import styled from 'styled-components'
+import img from './img/app_pg_one_bg.svg'
 
 function App() {
   return (
     <div id="root">
       <GlobalStyle />
-      <DiveLogs />
+      <BackgroundLog>
+        <DiveLogs />
+        <DiveLogs />
+        <DiveLogs />
+      </BackgroundLog>
     </div>
   )
 }
+
+const BackgroundLog = styled.div`
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  height: 100vh;
+  background-size: cover;
+  /* padding-top: 4px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
 
 export default App
