2df7cf224b099a4561452e65b4edbb6e8e76a46d
hagenbrandt <59567907+hagenbrandt@users.noreply.github.com>
2020-03-10 12:40:59 +0100

    Init firestore

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyD_2sEqRYzBOHvFvTg7c6myHpqYShWSxyc',
+  authDomain: 'thedivebook.firebaseapp.com',
+  databaseURL: 'https://thedivebook.firebaseio.com',
+  projectId: 'thedivebook',
+  storageBucket: 'thedivebook.appspot.com',
+  messagingSenderId: '683799707216',
+  appId: '1:683799707216:web:358e0b288c96039a87db55',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+export const logsRef = db.collection('logs')
