ae7f543f44474e647a28a15f4a0eefac69a2731c
hagenbrandt <59567907+hagenbrandt@users.noreply.github.com>
2020-03-19 14:17:36 +0100

    Set ratio to cam

--- src/camera/index.js
+++ src/camera/index.js
@@ -1,24 +1,48 @@
-import React, {useRef, useState} from 'react'
-import {useUserMedia} from '../hooks/use-user-media'
+import React, { useRef, useState } from 'react'
+import { Measure } from 'react-measure'
+import { useUserMedia } from '../hooks/useUserMedia'
+import { useCardRatio } from '../hooks/useCardRatio'
 
 const CAPTURE_OPTIONS = {
-    audio: false,
-    video: {facingMode: 'environment'},
+  audio: false,
+  video: { facingMode: 'environment' },
 }
 
-funciton Camera() {
-    const videoRef = useRef()
-    const mediaStream = useUserMedia(CAPTURE_OPTIONS)
+function Camera() {
+  const videoRef = useRef()
+  const mediaStream = useUserMedia(CAPTURE_OPTIONS)
+  const [container, setContainer] = useState({ height: 0 })
+  const [aspectRatio, setAspectRatio] = useCardRatio(1.586)
 
-    if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
-        videoRef.current.srcObject = mediaStream
-    }
+  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+    videoRef.current.srcObject = mediaStream
+  }
 
-    function handleCanPlay() {
-        videoRef.current.play()
-    }
+  function handleCanPlay() {
+    calculateRatio(videoRef.current.videoHeight, videoRef.current.videoWidth)
+    setAspectRatio(videoRef.current.videoHeight, videoRef.current.videoWidth)
+    videoRef.current.play()
+  }
 
-    return  (
-        <video ref={videoRef} onCanPlay={handleCanPlay} autoPlay playInline muted />
-    )
+  function handleResize(contentRect) {
+    setContainer({
+      height: Math.round(contentRect.bounds.width / aspectRatio),
+    })
+  }
+
+  return (
+    <Measure bounds onResize={handleResize}>
+      {({ measureRef }) => (
+        <div ref={measureRef} style={{ height: `${container.height}px` }}>
+          <video
+            ref={videoRef}
+            onCanPlay={handleCanPlay}
+            autoPlay
+            playInline
+            muted
+          />
+        </div>
+      )}
+    </Measure>
+  )
 }
