7b7e838e103c00c256f74f2d97f4f020fc0ca3e9
Hagen Brandt <h.brandt@steinberg.de>
2021-04-30 23:07:29 +0200

    Refactor: Move countDuration to src/helper/countDuration/

--- src/helper/countDuration/countDuration.js
+++ src/helper/countDuration/countDuration.js
@@ -1,7 +1,9 @@
-export default function countDuration(timeDateOne, timeDateTwo) {
+const countDuration = (timeDateOne, timeDateTwo) => {
   const firstTime = new Date(timeDateOne)
   const secondTime = new Date(timeDateTwo)
   const diff = new Date(secondTime.getTime() - firstTime.getTime())
 
   return diff.getUTCMinutes()
 }
+
+export default countDuration
