3417c0b9e93bd38ccc1409db147dd53ea6ea8e43
Hagen Brandt <h.brandt@steinberg.de>
2021-05-01 22:48:04 +0200

    Feat: Create SignUp component

---
+++ src/auth/SignUp.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { signInWithGoogle } from '../config/firebase/firebase'
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [error, setError] = useState(null);
+  const createUserWithEmailAndPasswordHandler = (event, email, password) => {
+    event.preventDefault();
+    setEmail("");
+    setPassword("");
+    setDisplayName("");
+  };
+  const onChangeHandler = event => {
+    const { name, value } = event.currentTarget;
+    if (name === "userEmail") {
+      setEmail(value);
+    } else if (name === "userPassword") {
+      setPassword(value);
+    } else if (name === "displayName") {
+      setDisplayName(value);
+    }
+  };
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <div>
+        {error !== null && (
+          <div>
+            {error}
+          </div>
+        )}
+        <form className="">
+          <label htmlFor="displayName" className="block">
+            Display Name:
+          </label>
+          <input
+            type="text"
+            name="displayName"
+            value={displayName}
+            placeholder="name"
+            id="displayName"
+            onChange={event => onChangeHandler(event)}
+          />
+          <label htmlFor="userEmail" className="block">
+            Email:
+          </label>
+          <input
+            type="email"
+            name="userEmail"
+            value={email}
+            placeholder="email"
+            id="userEmail"
+            onChange={event => onChangeHandler(event)}
+          />
+          <label htmlFor="userPassword">
+            Password:
+          </label>
+          <input
+            type="password"
+            name="userPassword"
+            value={password}
+            placeholder="Your Password"
+            id="userPassword"
+            onChange={event => onChangeHandler(event)}
+          />
+          <button
+            onClick={event => {
+              createUserWithEmailAndPasswordHandler(event, email, password);
+            }}
+          >
+            Sign up
+          </button>
+        </form>
+        <p>or</p>
+        <button
+          onClick={() => signInWithGoogle()}
+        >
+          Sign In with Google
+        </button>
+        <p>
+          Already have an account?{" "}
+          <Link to="/signIn">
+            Sign in here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default SignUp;
