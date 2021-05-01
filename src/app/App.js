46cbf334b615eddc33280a8f9a1ce4604854576c
Hagen Brandt <h.brandt@steinberg.de>
2021-05-01 22:54:15 +0200

    Feat: Add authorization to App

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, {useContext} from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import LogList from '../logList/LogList'
 import BackgroundLog from '../backgroundLog/BackgroundLog'
 import DetailLog from '../detailLog/DetailLog'
 import LogBook from '../logBook/Logbook'
 import { CameraSite } from '../camera/CameraSite'
 import Footer from '../footer/Footer'
+import SignIn from '../auth/SignIn'
+import SignUp from '../auth/SignUp'
+import PasswordReset from '../auth/PasswordReset'
+import {UserContext} from '../providers/UserProvider'
+import {auth} from '../config/firebase/firebase'
 
 export default function App() {
+  const user = useContext(UserContext);
   return (
+    !user ? <Router>
+        <Redirect to='/signIn'/>
+        <Route path="/signUp" component={SignUp} />
+        <Route path="/signIn" component={SignIn}/>
+        <Route path="/passwordReset" component={PasswordReset}/>
+      </Router> :
     <Router>
       <BackgroundLog>
+        <button onClick={() => auth.signOut()}>X</button>
         <Switch>
           <Route exact path="/" component={LogList} />
           <Route exact path="/DetailLog/:id" component={DetailLog} />
