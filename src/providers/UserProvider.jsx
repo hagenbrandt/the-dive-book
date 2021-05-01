59c88d7071e70780e748450b79108555ea1cdb87
Hagen Brandt <h.brandt@steinberg.de>
2021-05-02 00:26:20 +0200

    Refactor: Add generateUserDocument to DidMount in UserProvider

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,5 +1,5 @@
 import React, {Component, createContext} from 'react'
-import {auth} from '../config/firebase/firebase'
+import {auth, generateUserDocument} from '../config/firebase/firebase'
 
 export const UserContext = createContext({ user: null });
 class UserProvider extends Component {
@@ -7,9 +7,10 @@ class UserProvider extends Component {
     user: null
   };
 
-  componentDidMount = () => {
-    auth.onAuthStateChanged(userAuth => {
-      this.setState({ user: userAuth});
+  componentDidMount = async () => {
+    auth.onAuthStateChanged(async userAuth => {
+      const user = await generateUserDocument(userAuth);
+      this.setState({ user });
     });
   };
   render() {
