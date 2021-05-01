c6e34aa55406c3e2325b695e2a7771d3a1fced4d
Hagen Brandt <h.brandt@steinberg.de>
2021-05-02 00:20:02 +0200

    Feat: Create generateUserDocument and getUserDocument function

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -19,6 +19,8 @@ const db = firebase.firestore()
 
 export const storage = firebase.storage()
 
+export const firestore = firebase.firestore();
+
 export const logsRef = db.collection('logs')
 
 export const auth = firebase.auth()
@@ -27,4 +29,37 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
+};
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email, displayName, photoURL } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
 };
