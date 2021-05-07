f45e845f2a5d0792b449996fb5b10ff9efb6c130
Hagen Brandt <h.brandt@steinberg.de>
2021-05-08 01:13:59 +0200

    Feat: Create colors for styleguide

---
+++ src/styles/utils/colors.js
@@ -0,0 +1,31 @@
+export const blue = {
+  stratosLight: 'rgba(0, 15, 77, 1)',
+  stratos: '#001051',
+  stratosDark: '#000d41',
+  stratosDarker: '#000a31',
+  stratosGradientOne: 'rgba(0, 13, 65, 0)',
+  stratosGradientTwo: 'rgba(0, 13, 65, 0.75)',
+  resolutionBlueDefault: '#001c8c',
+  resolutionBlueDefaultDarker: '#001776',
+  resolutionBlueLight: '#001a83',
+  resolutionBlueMedium: '#001e97',
+  resolutionBlue: '#031e82',
+  resolutionBlueDark: '#00166f',
+  midnightBlue: '#001464',
+  internationalKleinBlueLighter: 'rgba(0, 37, 185, 0.2)',
+  internationalKleinBlueLight: 'rgba(0, 37, 185, 1)',
+  internationalKleinBlue: '#0020a2',
+  twilightBlueLighter: 'rgba(236, 252, 255, 0.3)',
+  twilightBlueLight: 'rgb(236, 252, 255, 0.8)',
+  twilightBlue: '#ECFCFF',
+  dodgerBlue: '#3e64ff',
+  dodgerBlueLight: 'rgba(62, 100, 255, 0.3)'
+}
+
+export const green = {
+  oceanGreen: '#41b883'
+}
+
+export const red = {
+  maroonFlush: '#bf1650'
+}
