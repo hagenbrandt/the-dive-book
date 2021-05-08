123f34db06351d63faf279bd9009c8aa9fd719c2
Hagen Brandt <h.brandt@steinberg.de>
2021-05-09 00:57:20 +0200

    Refactor: Remove unused imports

--- src/app/App.js
+++ src/app/App.js
@@ -7,8 +7,6 @@ import LogBook from '../logBook/Logbook'
 import { CameraSite } from '../camera/CameraSite'
 import Footer from '../footer/Footer'
 import SignIn from '../auth/SignIn'
-import SignUp from '../auth/SignUp'
-import PasswordReset from '../auth/PasswordReset'
 import {UserContext} from '../providers/UserProvider'
 import {auth} from '../config/firebase/firebase'
 
@@ -17,9 +15,7 @@ export default function App() {
   return (
     <Router>
       {!user ? <Redirect to='/signIn'/> : null}
-        <Route path="/signUp" component={SignUp} />
         <Route path="/signIn" component={SignIn}/>
-        <Route path="/passwordReset" component={PasswordReset}/>
       <BackgroundLog>
         <button onClick={() => auth.signOut()}>X</button>
         <Switch>
