50fa7de302aebbc2de0b6c9c38e9f48560866997
Hagen Brandt <h.brandt@steinberg.de>
2021-05-08 21:44:28 +0200

    Refactor: Adjust GlobalStyles and use variables from colors and typography

--- src/styles/common/GlobalStyles.js
+++ src/styles/common/GlobalStyles.js
@@ -1,20 +1,53 @@
 import { createGlobalStyle } from 'styled-components'
+import { primaryFont, typeScale } from '../utils/typography'
 
 const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        max-width: 100%;
+        overflow-x: hidden;
+        position: relative;
+        display: block;
     }
+    
     #root {
         height: 100%;
     }
+    
     body {
         display: grid;
-        margin: 0;
         height: 100vh;
         width: 100vw;
-        font-family: "Sonorous", sans-serif;
+        font-family: ${primaryFont};
         color: #000d41;
     }
+    
+    h1, h2 {
+        font-size: ${typeScale['header1']};
+    }
+    
+    h3 {
+        font-size: ${typeScale['header2']};
+    }
+    
+    h4 {
+        font-size: ${typeScale['header3']};
+    }
+    
+    h6 {
+        font-size: ${typeScale['header4']};
+    }
+    
+    p {
+        font-size: ${typeScale['paragraph']};
+    }
+    
+    span, li {
+        font-size: ${typeScale['tagElement']};
+    }
+    
     a {
         text-decoration: none;
     }
