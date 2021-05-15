17afeedde7138fd6727ef56afd7107e9ee69f8c2
Hagen Brandt <h.brandt@steinberg.de>
2021-05-15 22:21:49 +0200

    Style: Create global typography

---
+++ src/styles/utils/typography.js
@@ -0,0 +1,10 @@
+export const primaryFont = '"Sonorous", sans-serif';
+
+export const typeScale = {
+  header1: '1.5rem',
+  header2: '1rem',
+  header3: '0.75rem',
+  header4: '0.625rem',
+  paragraph: '0.625rem',
+  tagElement: '0.875rem'
+}
