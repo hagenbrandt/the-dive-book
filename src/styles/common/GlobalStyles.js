39f505b0df5136283a9648fc64f28d0f3a728673
Hagen Brandt <h.brandt@steinberg.de>
2021-05-15 22:37:28 +0200

    Style: Adjust global styles and use global typography

--- src/styles/common/GlobalStyles.js
+++ src/styles/common/GlobalStyles.js
@@ -1,22 +1,54 @@
 import { createGlobalStyle } from 'styled-components'
+import { primaryFont, typeScale } from '../utils/typography'
 
 const GlobalStyle = createGlobalStyle`
+    
     * {
         box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        max-width: 100%;
     }
+    
     #root {
         height: 100%;
     }
+    
     body {
         display: grid;
         margin: 0;
         height: 100vh;
         width: 100vw;
-        font-family: "Sonorous", sans-serif;
+        font-family: ${primaryFont};
         color: #000d41;
     }
+    
     a {
         text-decoration: none;
+    }
+    
+    h1, h2 {
+        font-size: ${typeScale['header1']};
+    }
+    
+    h3 {
+        font-size: ${typeScale['header2']};
+    }
+    
+    h4 {
+        font-size: ${typeScale['header3']};
+    }
+    
+    h6 {
+        font-size: ${typeScale['header4']};
+    }
+    
+    p {
+        font-size: ${typeScale['paragraph']};
+    }
+    
+    span, li {
+        font-size: ${typeScale['tagElement']};
     }
     `
 export default GlobalStyle
